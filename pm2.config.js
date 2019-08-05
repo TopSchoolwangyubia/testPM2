d3bb4c66708a73a4fac4cc0e2d78fa0f90f60a83
wang yu biao <1443937536@qq.com>
2019-08-05 12:31:32 +0800

    m.pm2 config

--- pm2.config.js
+++ pm2.config.js
@@ -1,16 +1,11 @@
 const path = require("path");
+const host = "47.93.194.56";
 module.exports = {
   apps: [
     {
       name: "app",
       script: "./server/index.js",
       cwd: path.resolve(__dirname, "./"), // 当前工作路径
-      env: {
-        NODE_ENV: "development"
-      },
-      env_production: {
-        NODE_ENV: "production"
-      },
       max_memory_restart: "10000M",
       ignore_watch: [
         // 从监控目录中排除
@@ -18,7 +13,7 @@ module.exports = {
         "logs",
         "static"
       ],
-      watch: [path.resolve(__dirname, "./server/")],
+      watch: ["server"],
       out_file: "./logs/out.log", // 普通日志路径
       error_file: "./logs/err.log", // 错误日志路径
       merge_logs: false,
@@ -28,29 +23,17 @@ module.exports = {
   deploy: {
     // "production" is the environment name
     production: {
-      // 服务器端SSH key 地址, 默认 $HOME/.ssh
-      key: "$HOME/.ssh",
       // SSH user 需配置
       user: "root",
       // SSH host 需配置
-      host: ["47.93.157.56"],
-      // SSH options with no command-line flag, see 'man ssh'
-      // can be either a single string or an array of strings
-      ssh_options: "StrictHostKeyChecking=no",
+      host: [ host ],
       // GIT remote/branch
       ref: "origin/master",
       // GIT ssh远程地址 需配置
-      repo: "git@github.com:raoenhui/next-site-cn.git",
+      repo: "git@github.com/TopSchoolwangyubia/testPM2.git",
       // 放入服务器端地址 需配置
-      path: "/ice/pm2",
-      // Pre-setup command or path to a script on your local machine
-      "pre-setup": "echo 'This is a pre-setup command'",
-      // Post-setup commands or path to a script on the host machine
-      // eg: placing configurations in the shared dir etc
-      "post-setup": "ls -la",
-      // pre-deploy action
-      "pre-deploy-local": "echo 'This is a pre-deploy-local command'",
+      path: "/usr/local/src/testPM2",
       // post-deploy action
-      "post-deploy": "npm install && pm2 reload pm2.config.js --env production'"
+      "post-deploy": "npm install && pm2 reload pm2.config.js",
     },
 };
